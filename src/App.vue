c15918e7848d800f8ea0ccadabfed25562dc2156
hxius <bebe1757@163.com>
2022-07-16 15:59:29 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,63 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <router-view></router-view>
+  <div class="docker">
+    <div class="docker__item docker__item--active">
+      <i class="iconfont icon-homefill"></i>
+      <div class="title">首页</div>
+    </div>
+    <div class="docker__item">
+      <i class="iconfont icon-cart"></i>
+      <div class="title">购物车</div>
+    </div>
+    <div class="docker__item">
+      <i class="iconfont icon-order"></i>
+      <div class="title">订单</div>
+    </div>
+    <div class="docker__item">
+      <i class="iconfont icon-my"></i>
+      <div class="title">我的</div>
+    </div>
+  </div>
 </template>
+<!-- https://www.fastmock.site/mock/ae8e9031947a302fed5f92425995aa19/jd/api/user/register -->
+
+<script>
+import { reactive } from 'vue'
+export default {
+  name: 'App',
+  setup() {
+    const navList = reactive([])
+    return { navList }
+  }
+}
+</script>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+.docker {
+  display: flex;
+  align-items: center;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  padding: 0 0.18rem;
+  box-sizing: border-box;
+  height: 0.49rem;
+  width: 100%;
+  border-top: 1px solid #f1f1f1;
+  .docker__item {
+    flex: 1;
+    text-align: center;
+    &.docker__item--active {
+      color: #1fa4fc;
+    }
+    .iconfont {
+      display: block;
+      font-size: 0.18rem;
+    }
+    .title {
+      font-size: 20px;
+      transform: scale(0.5, 0.5);
+      transform-origin: center;
     }
   }
 }
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="docker"></div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView'
+}
+</script>
+
+<style lang="less" scoped></style>
